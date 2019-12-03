8159432dd72f74e2094ffa332e94af4c6ea40d2b
Isac Kim <jk724814@gmail.com>
2019-12-03 16:21:27 +0100

    Features: almost done with the user profile. I have added the pet cards for each user, fixed the create page for animals, added location and availability to the create animals as well

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,10 @@
 
 Animal.destroy_all
 User.destroy_all
+
 @elisa = User.new(name: "Elisa", email: "elisa.vanderperre@hotmail.com")
 @elisa.save
-@bob = Animal.new(name: "Bob",species: "dog",availability: true,location: "Belgium",photo: "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500")
+@bob = Animal.new(name: "Bob",species: "dog",availability: true, location: "Belgium",photo: "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500")
 @bob.user = @elisa
 @bob.save
 
@@ -25,4 +26,5 @@ User.destroy_all
 @garfield.save
 
 
-p "#{Animal.count} animals created"
+p "#{Animal.count} users created"
+p "#{User.count} animals created"

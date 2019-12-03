0e9c7de516f9367fcffc836cb042f38abf772c34
Elisa Van der Perre <elisa.vanderperre@hotmail.com>
2019-12-03 13:03:29 +0100

    Adding animal show

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -2,4 +2,10 @@ class AnimalsController < ApplicationController
   def index
     @animals = Animal.all
   end
+
+  def show
+    @animal = Animal.find(params[:id])
+    @user = current_user
+    @booking = Booking.new
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@elisa = User.new(name: "Elisa", email: "elisa.vanderperre@hotmail.com")
+@elisa.save
+
+@bob = Animal.new(name: "Bob",species: "dog",availability: true,location: "Belgium",photo: "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500")
+@bob.user = @elisa
+@bob.save
+
+@garfield = Animal.new(
+  name: "Garfield",
+  species: "cat",
+  availability: true,
+  location: "Spain",
+  photo: "https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png"
+)
+@garfield.user = @elisa
+@garfield.save
+
+p "#{Animal.count} animals created"

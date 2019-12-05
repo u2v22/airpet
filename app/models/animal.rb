f7bb7f1bc06cec98a4e5cd2f8e1f8d0617e55af3
Isac Kim <jk724814@gmail.com>
2019-12-05 15:44:54 +0100

    fixed geoceded lines from animal models added again

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -6,4 +6,7 @@ class Animal < ApplicationRecord
   validates :species, :name, :location, :availability_start, :availability_end, :photo, presence: true
   SPECIES = ["Dog", "Cat", "Bird", "Hamster"]
   validates :species, inclusion: { in: SPECIES }
+
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
